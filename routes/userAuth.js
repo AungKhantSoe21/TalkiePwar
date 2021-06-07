7dc22c6a785e3139b966278e8d458769c02c7acd
Zaw Min Tun <zawmintun.tp@gmail.com>
2021-06-07 13:25:59 +0630

    update register

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 var router = express.Router();
+const md5 = require('md5');
+const suffix = 'sittwe'
+
+// User Model
+const authModel = require('../model/auth');
 
 router.get('/register', (req, res, next) => {
     res.render('register', {
@@ -13,25 +18,55 @@ router.get('/login', (req, res, next) => {
 
 router.post('/register', (req, res) => {
     const { user_name, phone_number, password, password2 } = req.body;
-    
+    let errors = [];
+
     if(!user_name || !phone_number || !password || !password2){
-        return res.render('register', {
-            error : "Please fill in all fields"
-        })
+        errors.push({ msg: 'Please fill in all fields' })
     }
 
     if(password.length < 6){
-        return res.render('register', {
-            error : "Password should be at least 6 characters"
-        })
+        errors.push({ msg: 'Password should be at least 6 characters' })
     }
 
     if(password != password2){
-        return res.render('register', {
-            error : "Passwords do not match"
-        })
+        errors.push({ msg: 'Passwords do not match' })
+    }
+
+    if(errors.length > 0 ) {
+        res.render('register', { 
+            errors,
+            user_name,
+            phone_number,
+            password,
+            password2
+         })
+    } else {
+        // Validation passed
+        authModel.findOne({ phone_number: phone_number })
+        .then(user => {
+            console.log(user)
+            if(user) {
+                // User exits
+                errors.push({ msg: 'Phone number is already registered' })
+                res.render('register', { 
+                    errors,
+                    user_name,
+                    phone_number,
+                    password,
+                    password2
+                 })
+            } else {
+                const newUser = new user ({
+                    user_name,
+                    phone_number,
+                    password
+                })
+
+                console.log(newUser)
+            }
+        });
     }
     
-})
+});
 
 module.exports = router;
