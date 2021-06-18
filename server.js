87e06f93e853cb776ff24e9874fd4417b8cbca54
AungKhantSoe21 <aksaksaks96@gmail.com>
2021-06-18 17:41:36 +0630

    error page update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyparser = require ('body-parser');
+const createError = require('http-errors')
 
 // DB config
 const db = require('./routes/dbCreate');
@@ -38,6 +39,22 @@ app.locals.title = "TalkiePwar"
 
 app.use('/', userAuthRouter);
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 const port = 8000 || process.env.port;
 
